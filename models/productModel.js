f38bcadef66beb90526f9d18c24b77e8be20d22b
sana-sadiq <109807783+sana-sadiq@users.noreply.github.com>
2024-05-10 12:37:42 +0530

    Create productModel.js

---
+++ models/productModel.js
@@ -0,0 +1,11 @@
+// productModel.js
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  quantity: { type: Number, required: true },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
